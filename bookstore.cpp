742eb455dbb30a883f3ae2cdb968979158ed3f07
lusm <loveyousomuch554@gmail.com>
2022-07-07 01:27:31 +0300

    print

---
+++ bookstore.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+  std::cout << "Enter two numbers:" << std::endl;
+  int v1 = 0, v2 = 0;
+  std::cin >> v1 >> v2;
+  std::cout << "Sum of 'v1', 'v2': " << v1 + v2 << std::endl;
+
+  return 0;
+}
+
+
+
