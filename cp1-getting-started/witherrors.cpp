496bfb8d55dbce60a2cbe7d70792a7220a222ee3
lusm <loveyousomuch554@gmail.com>
2022-07-11 20:31:39 +0300

    errors

---
+++ cp1-getting-started/witherrors.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(void) {
+  /* Syntax error */
+  //std::cout << "Hello, World" << std::endl // <- ";" here
+
+  /* Type errors */
+  //int a = "";
+
+  /* Declaration error */
+  //std::cout << test << std::endl;
+  return 0;
+}
