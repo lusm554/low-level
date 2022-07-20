fcaba11a827b28906f133d1e167f2c4f9dda7d64
lusm <loveyousomuch554@gmail.com>
2022-07-20 22:35:48 +0300

    pointer

--- cp2-vars-basic-types/test.cpp
+++ cp2-vars-basic-types/test.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 
 int main(void) {
-  int val = 1024;
-  int *pi = &val;
-  int **pp = &pi; 
+  int null = 0, *p = 0;
 
-  std::cout << val << " " << *pi << std::endl;
-  std::cout << **pp << std::endl;
 
   return 0;
 }
