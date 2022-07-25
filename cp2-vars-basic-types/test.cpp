f85cfbc00e94fbe79cd3060c09e27dd6e77c0e6e
lusm <loveyousomuch554@gmail.com>
2022-07-26 00:02:52 +0300

    decltype

--- cp2-vars-basic-types/test.cpp
+++ cp2-vars-basic-types/test.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 
 int main(void) {
-  int null = 0, *p = 0;
+  int a = 3, b = 4;
+  decltype(a) c = a;
+  decltype((b)) d = a;
 
+  std::cout << c << std::endl;
+  std::cout << d << std::endl;
+  ++c;
+  ++d;
+
+  std::cout << c << std::endl;
+  std::cout << d << std::endl;
 
   return 0;
 }
