a8feeb54dd888181eb2fdd5ef1900474b35cd4c0
lusm <loveyousomuch554@gmail.com>
2022-07-29 00:49:34 +0300

    init

---
+++ cp2-vars-basic-types/sales-data-2-41.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main(void) {
+  struct Sales_data {
+    int a;
+  };
+
+  Sales_data data;
+
+  std::cin >> data.a;
+  std::cout << data.a << std:
+
+  return 0; 
+}
