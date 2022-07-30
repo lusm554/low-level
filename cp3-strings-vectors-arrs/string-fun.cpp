1175023bb94dd8beb72c112d971618db4b738d19
lusm <loveyousomuch554@gmail.com>
2022-07-30 17:52:46 +0300

    get line

--- cp3-strings-vectors-arrs/string-fun.cpp
+++ cp3-strings-vectors-arrs/string-fun.cpp
@@ -5,11 +5,12 @@ using std::string;
 using std::cout; using std::cin; using std::endl;
 
 void read_words(void);
+void get_line(void);
 
 int main(void)
 {
-  read_words();
-
+  //read_words();
+  get_line();
   return 0;
 }
 
@@ -19,3 +20,11 @@ void read_words(void)
   while (cin >> word)
     cout << word << endl;
 }
+
+void get_line(void)
+{
+  string line;
+  while (getline(cin, line))
+    cout << line << endl;
+}
+
