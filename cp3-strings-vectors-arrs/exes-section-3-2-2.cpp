4b9ad6b4740aaf2beb2934584029d29869e21557
lusm <loveyousomuch554@gmail.com>
2022-07-31 17:38:15 +0300

    check sizes and chars

--- cp3-strings-vectors-arrs/exes-section-3-2-2.cpp
+++ cp3-strings-vectors-arrs/exes-section-3-2-2.cpp
@@ -5,10 +5,12 @@ using std::cout; using std::cin; using std::endl;
 using std::string;
 
 void ex2();
+void ex4();
 
 int main(void)
 {
-  ex2();
+  //ex2();
+  ex4();
   return 0;
 }
 
@@ -25,4 +27,24 @@ void ex2()
   string word;
   while (cin >> word)
     cout << word << endl;
+}
+
+void ex4()
+{
+  string str1, str2;
+
+  getline(cin, str1);
+  getline(cin, str2); 
+
+  // first version
+  if (str1 == str2)
+    cout << "str1 & str2 equal." << endl;
+  else // if not equal check which *larger* 
+    cout << ((str1 > str2) ? str1 : str2) << endl;
+
+  // second version 
+  if (str1.size() == str2.size())
+    cout << "str1 & str2 have the same len." << endl;
+  else // if not equal len check which str longer
+    cout << (str1.size() > str2.size() ? str1 : str2) << endl;
 }
