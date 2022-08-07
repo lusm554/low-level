f376bbde341b205d5364b91d9226f634eed4efb5
lusm <loveyousomuch554@gmail.com>
2022-08-07 15:04:43 +0300

    change chars through while & traditional for

--- cp3-strings-vectors-arrs/3-2-section.cpp
+++ cp3-strings-vectors-arrs/3-2-section.cpp
@@ -5,10 +5,12 @@ using std::cout; using std::cin; using std::endl;
 using std::string;
 
 void ex3_6();
+void ex3_8();
 
 int main()
 {
-  ex3_6();
+  //ex3_6();
+  ex3_8();
   return 0;
 }
 
@@ -19,4 +21,20 @@ void ex3_6()
     c = 'X';
 
   cout << str << endl;
+}
+
+void ex3_8()
+{
+  string wstr = "hello world"; 
+  string::size_type n = 0;
+  while (n < wstr.size())
+    wstr[n++] = 'X';
+
+  cout << wstr << endl;
+
+  string fstr = "hello world";
+  for (string::size_type i = 0; i < fstr.size(); i++)
+    fstr[i] = 'X';
+
+  cout << fstr << endl; 
 }
