990b738239eab2413c5f93c0b6ef0f7fba265a10
lusm <loveyousomuch554@gmail.com>
2022-08-09 18:51:37 +0300

    ten ints vector

--- cp3-strings-vectors-arrs/3-4-section.cpp
+++ cp3-strings-vectors-arrs/3-4-section.cpp
@@ -5,10 +5,12 @@
 using namespace std;
 
 void upper();
+void ten_it();
 
 int main()
 {
-  upper();
+  //upper();
+  ten_it();
   return 0;
 }
 
@@ -26,5 +28,16 @@ void upper()
   
   for (auto line : text)
     cout << line << endl;
+}
+
+void ten_it()
+{
+  vector<int> teni;
+  for (int i = 0; i < 10; i++) teni.push_back(i);
+
+  for(auto it = teni.begin(); it != teni.end(); it++) {
+    *it*=2;
+    cout << *it << endl;
+  }
 }
 
