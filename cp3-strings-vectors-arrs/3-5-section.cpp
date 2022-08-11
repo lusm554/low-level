340356b565060fc1a0dd4d4556f8b02456a5d441
lusm <loveyousomuch554@gmail.com>
2022-08-11 20:38:21 +0300

    fill w vec

--- cp3-strings-vectors-arrs/3-5-section.cpp
+++ cp3-strings-vectors-arrs/3-5-section.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 void define_arr();
+void same_w_vec();
 
 int main()
 {
-  define_arr();
+  //define_arr();
+  same_w_vec();
   return 0;
 }
 
@@ -14,6 +17,13 @@ int main()
 void define_arr()
 {
   int arr[10];
+  for (int i = 0; i < 10; i++)
+    arr[i] = i;
+}
+
+void same_w_vec()
+{
+  vector<int> arr(10);
   for (int i = 0; i < 10; i++)
     arr[i] = i;
 }
