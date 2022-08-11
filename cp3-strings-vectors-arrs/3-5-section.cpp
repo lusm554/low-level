9b0903406601cfec07c4326f68c97ae7e5e721a8
lusm <loveyousomuch554@gmail.com>
2022-08-12 00:30:50 +0300

    set elems to zero

--- cp3-strings-vectors-arrs/3-5-section.cpp
+++ cp3-strings-vectors-arrs/3-5-section.cpp
@@ -5,11 +5,13 @@ using namespace std;
 
 void define_arr();
 void same_w_vec();
+void elems2zero();
 
 int main()
 {
   //define_arr();
-  same_w_vec();
+  //same_w_vec();
+  elems2zero();
   return 0;
 }
 
@@ -28,3 +30,16 @@ void same_w_vec()
     arr[i] = i;
 }
 
+void elems2zero()
+{
+  int arr[10];
+  for (int i = 0; i < 10; i++)
+    arr[i] = i;
+
+  int *b = begin(arr), *e = end(arr);
+  while (b != e)
+    *b++ = 0; 
+
+  for (auto i : arr)
+    cout << i << endl;
+}
