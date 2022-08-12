52680f3bc2531406691aa14cfa7d6b110a2b269c
lusm <loveyousomuch554@gmail.com>
2022-08-12 16:42:48 +0300

    compare vectors

--- cp3-strings-vectors-arrs/3-5-section.cpp
+++ cp3-strings-vectors-arrs/3-5-section.cpp
@@ -48,6 +48,7 @@ void elems2zero()
 
 void iseq()
 {
+  /*
   int arr1[5] = {1, 2, 3, 4, 5};
   int arr2[] = {1, 2, 3, 4, 4};
 
@@ -59,6 +60,25 @@ void iseq()
   }
   for (int i = 0; i < sizeof(arr1)/sizeof(int); i++) {
     if (arr1[i] != arr2[i]) {
+      cout << "not equal" << endl;
+      return;
+    }
+  }
+  cout << "equal" << endl;
+  */
+
+  // same with vectors
+  vector<int> v1(10, 1);
+  vector<int> v2(10, 2);
+  
+  if (v1.size() != v2.size()) {
+    cout << "not equal" << endl;
+    return;
+  }
+
+  for (auto it1 = v1.cbegin(), it2 = v2.cbegin();
+      it1 != v1.cend() && it2 != v2.cend(); it1++, it2++) {
+    if (*it1 != *it2) {
       cout << "not equal" << endl;
       return;
     }
