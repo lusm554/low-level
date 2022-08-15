a7010b0df8e7b0010225f978a7cbf5b7ac4284fd
lusm <loveyousomuch554@gmail.com>
2022-08-16 00:34:26 +0300

    operators

---
+++ cp4-expressions/4-2-section.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  int a = 12 / 3 * 4 + 5 * 15 + 24 % 4 / 2;  
+  // 16 + 75 + 24 % 4 / 2
+  // 16 + 75 = 91
+  cout << a << endl;
+  return 0;
+}
