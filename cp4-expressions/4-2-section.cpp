e1a4115c2d68e35dcd5d4c6411685cdb245dceb2
lusm <loveyousomuch554@gmail.com>
2022-08-16 00:41:14 +0300

    expression to determine even or odd

--- cp4-expressions/4-2-section.cpp
+++ cp4-expressions/4-2-section.cpp
@@ -6,9 +6,17 @@ using namespace std;
 
 int main()
 {
+  //4.4
   int a = 12 / 3 * 4 + 5 * 15 + 24 % 4 / 2;  
   // 16 + 75 + 24 % 4 / 2
   // 16 + 75 = 91
   cout << a << endl;
+
+  // 4.6
+  int ival = 11;
+  bool iseven = ival % 2 == 0;
+  cout << (iseven ? "even" : "odd") << endl;
   return 0;
+
+
 }
