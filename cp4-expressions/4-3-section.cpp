078f0ff7f21d06a16c92ab41634a963bf30885ea
lusm <loveyousomuch554@gmail.com>
2022-08-16 19:46:34 +0300

    4.8 logical operators

---
+++ cp4-expressions/4-3-section.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+
+  // 4.8
+  // con1 && con2, con2 will evaluated if con1 is true
+  // con1 || con2, con2 will evaluated if con1 is false 
+  // con1 > con2, con1 and con2 casting to the same type
+  // con1 > con2 > con3, first con1 and con2 returns bool, then bool compared with con3.
+  // The best way ==> con1 > con2 && con2 > con3.
+  // val == true, bool 'true' cast to the same value as 'val'. So if val is int, then val == 1.
+  
+
+}
+
+
