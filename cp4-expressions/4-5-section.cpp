0faf27f1700ae44c6d8944b8debfc35e030532e2
lusm <loveyousomuch554@gmail.com>
2022-08-18 14:00:00 +0300

    increment & decrement

---
+++ cp4-expressions/4-5-section.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  // 4.17 
+  // ++a - increment object a
+  // a++ - return copy of object a and increment origin object a
+
+  // 4.19
+  vector<int> vec = {1, 2, 3};
+  int ival = 0;
+
+
+  return 0;
+}
