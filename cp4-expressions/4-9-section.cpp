7849275e30c1541e3e01a5fe0baa0646d7c1d379
lusm <loveyousomuch554@gmail.com>
2022-08-20 20:15:37 +0300

    print size of each built-in types

---
+++ cp4-expressions/4-9-section.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  //4.28
+  cout << "short int " << sizeof(short int) << endl;
+  cout << "int " << sizeof(int) << endl;
+  cout << "long int " << sizeof(long int) << endl;
+  cout << "long long int " << sizeof(long long int) << endl;
+
+  cout << "char " << sizeof(char) << endl;
+  cout << "bool " << sizeof(bool) << endl;
+  cout << "double " << sizeof(double) << endl;
+  cout << "long double " << sizeof(long double) << endl;
+  cout << "float " << sizeof(float) << endl;
+
+  return 0;
+}
