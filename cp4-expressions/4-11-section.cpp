20a5b3efd9362e1758f07ef02c4af44738fe7778
lusm <loveyousomuch554@gmail.com>
2022-08-21 16:30:45 +0300

    new style cast

--- cp4-expressions/4-11-section.cpp
+++ cp4-expressions/4-11-section.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+  /*
   // 4.36
   int i = 2;
   double d = .5;
@@ -11,6 +13,30 @@ int main()
   //i *= (int)d; // old-style cast
   i *= static_cast<int>(d); // new style
   cout << i << endl;
+  */
+
+  // 4.37
+  int i;
+  double d;
+  const string *ps;
+  char *pc;
+  void *pv;
+
+  // a
+  // pv = (void*)ps;
+  pv = static_cast<void*>(const_cast<string*>(ps));
+
+  // b
+  // i = int(*pc);
+  i = static_cast<int>(*pc);
+
+  // c
+  // pv = &d;
+  pv = &d;
+
+  // d
+  // pc = (char*) pv;
+  pc = reinterpret_cast<char*>(pv);
 
   return 0;
 }
