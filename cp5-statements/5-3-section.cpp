478f1e2537c82a72e29a5bf9c350e61eb8ac7f29
lusm <loveyousomuch554@gmail.com>
2022-08-23 22:49:07 +0300

    input & letter grade with cond operator

--- cp5-statements/5-3-section.cpp
+++ cp5-statements/5-3-section.cpp
@@ -5,10 +5,12 @@
 using namespace std;
 
 void letter_grade();
+void letter_grade_ter();
 
 int main()
 {
-  letter_grade();
+  //letter_grade();
+  letter_grade_ter();
 
   return 0;
 }
@@ -30,3 +32,15 @@ void letter_grade()
   cout << lettergrade << endl;
 }
 
+void letter_grade_ter()
+{
+  vector<string> scores = {"F", "D", "C", "B", "A", "A++"};
+  string lettergrade;
+
+  int grade;
+  while (cin >> grade && grade <= 100) {
+    lettergrade += grade < 60 ? scores[0] : scores[(grade - 50)/10];
+  }
+
+  cout << lettergrade << endl;
+}
