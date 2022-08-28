1c11ccb38b75582590e427af3ed8969ff483dbb6
lusm <loveyousomuch554@gmail.com>
2022-08-28 20:55:41 +0300

    skip [0] != "_"

--- cp5-statements/5-5-section.cpp
+++ cp5-statements/5-5-section.cpp
@@ -5,10 +5,12 @@
 using namespace std;
 
 void check_in();
+void check_in_cont();
 
 int main()
 {
-  check_in();
+  //check_in();
+  check_in_cont();
 
   return 0;
 }
@@ -31,3 +33,24 @@ void check_in()
   if (!isrepeated)
     cout << "No word repeated." << endl;
 }
+
+void check_in_cont()
+{
+  string in, previn;
+  bool isrepeated = false;
+
+  while (cin >> in) {
+    if (in[0] != '_')
+      continue;
+    if (in == previn) {
+      cout << "Word " << in << " occurs 2 times." << endl;
+      isrepeated = true;
+      break;
+    }
+    previn = in;
+  }
+
+  if (!isrepeated)
+    cout << "No word repeated." << endl;
+}
+
