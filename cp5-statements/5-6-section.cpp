34fd5107629865c853c79e1229226352fb4c10e2
lusm <loveyousomuch554@gmail.com>
2022-08-29 23:41:10 +0300

    throw exception if the second num is zero

--- cp5-statements/5-6-section.cpp
+++ cp5-statements/5-6-section.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 void div_ints();
+void zero_exception();
 
 int main()
 {
-  div_ints();
+  //div_ints();
+  zero_exception();
 
   return 0;
 }
@@ -24,3 +26,23 @@ void div_ints()
 
   cout << "Div result: " << result << endl;
 }
+
+void zero_exception()
+{
+  try {
+    int n1, n2, result;
+
+    cout << "Input two numbers: ";
+    if (cin >> n1 >> n2) {
+      if (n2 == 0) {
+        throw runtime_error("Second number should no equal zero");
+      }  
+      result = n1 / n2;
+    }
+    
+    cout << "Div result: " << result << endl;
+  } catch (runtime_error err) {
+    cout << "Error: " << err.what() << endl;
+  }
+}
+
