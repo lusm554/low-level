5b4361234f8e0398afd3c1ce67a79ceadcacd681
lusm <loveyousomuch554@gmail.com>
2022-08-31 19:01:08 +0300

    func that ask for input num

--- cp6-functions/6-1-section.cpp
+++ cp6-functions/6-1-section.cpp
@@ -3,15 +3,32 @@
 using namespace std;
 
 int fact(int val);
+int get_fact(void);
 
 int main()
 {
   int fact5 = fact(5);
   cout << fact5 << endl;
+
+  int fact = get_fact();
+  cout << fact << endl;
 }
 
 int fact(int val)
 {
+  int res = 1;
+  while (val > 1)
+    res *= val--;
+
+  return res;
+}
+
+int get_fact(void)
+{
+  int val;
+  if (!(cin >> val))
+    throw runtime_error("Bad input to get_fact");
+
   int res = 1;
   while (val > 1)
     res *= val--;
