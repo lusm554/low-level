2825073023c968615545f375bc1d56ef2eeab0b0
lusm <loveyousomuch554@gmail.com>
2022-09-04 15:17:12 +0300

    initializer_list sum

--- cp6-functions/6-2-5-section.cpp
+++ cp6-functions/6-2-5-section.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 #include <string>
+#include <initializer_list>
 
 using namespace std;
 
-int main(int argc, char** argv) {
+int sum_lst(initializer_list<int> lst);
+
+int main(int argc, char** argv) 
+{
   /*
   string res = "";
 
@@ -13,8 +17,24 @@ int main(int argc, char** argv) {
   cout << res << endl;
   */
 
+  /*
   for (int i = 0; i < argc; i++)
     cout << i << ":\t" << argv[i] << endl;
+  */
+
+  initializer_list<int> list = {1, 2, 3};
+  cout << sum_lst(list) << endl;
 
   return 0;
 }
+
+int sum_lst(initializer_list<int> lst)
+{
+  int sum = 0;
+  for (auto i : lst)
+    sum += i;
+  
+  return sum;
+}
+
+
