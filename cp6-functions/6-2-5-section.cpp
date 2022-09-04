29de961d2ef432aecfb0db88eca52ab1040510cb
lusm <loveyousomuch554@gmail.com>
2022-09-04 14:28:53 +0300

    print options passed to main

--- cp6-functions/6-2-5-section.cpp
+++ cp6-functions/6-2-5-section.cpp
@@ -4,12 +4,17 @@
 using namespace std;
 
 int main(int argc, char** argv) {
+  /*
   string res = "";
 
   for (int i = 1; i < argc; i++) 
     res += argv[i] + string(" ");
 
   cout << res << endl;
+  */
+
+  for (int i = 0; i < argc; i++)
+    cout << i << ":\t" << argv[i] << endl;
 
   return 0;
 }
