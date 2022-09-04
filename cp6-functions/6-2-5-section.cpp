663c50dfcd4199e2fbddc119dd506916e780a8bc
lusm <loveyousomuch554@gmail.com>
2022-09-04 14:25:53 +0300

    concat arg strings

---
+++ cp6-functions/6-2-5-section.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+  string res = "";
+
+  for (int i = 1; i < argc; i++) 
+    res += argv[i] + string(" ");
+
+  cout << res << endl;
+
+  return 0;
+}
