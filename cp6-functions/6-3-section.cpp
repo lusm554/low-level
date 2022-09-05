e4f7cfe6d83c5f4e081c37bd670fc2d9a4ebfc95
lusm <loveyousomuch554@gmail.com>
2022-09-05 22:22:25 +0300

    return pointer to arr of ten things

--- cp6-functions/6-3-section.cpp
+++ cp6-functions/6-3-section.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
 
 void rec_print(vector<int> &vec, int i);
+string (*get_10_s())[10];
+string ss[10] = {"hello", "world"};
 
 int main()
 {
   // 6.33, recursive print 
   vector<int> vec = { 1, 2, 3, 4, 5 };
   rec_print(vec, vec.size() - 1);
+
+
 
   return 0;
 }
@@ -23,3 +28,9 @@ void rec_print(vector<int> &vec, int i)
   rec_print(vec, i - 1);
   cout << vec[i] << endl;
 }
+
+string (*get_10_s())[10] 
+{
+  return &ss;
+}
+
