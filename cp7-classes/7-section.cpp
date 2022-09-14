f9841f5334a95aa7cfa993b3409482be75adf15a
lusm <loveyousomuch554@gmail.com>
2022-09-14 23:36:09 +0300

    trans processing prog

---
+++ cp7-classes/7-section.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Sales_data
+{
+  string bookNo;
+  unsigned units_sold = 0;
+  double revenue = .0;
+};
+
+int main()
+{
+  Sales_data total;
+  if (cin >> total.bookNo >> total.units_sold >> total.revenue)
+  {
+    Sales_data trans;
+    while (cin >> trans.bookNo >> trans.units_sold >> trans.revenue)
+    {
+      if (total.bookNo == trans.bookNo)
+      {
+        total.units_sold += trans.units_sold;
+        total.revenue += trans.revenue;
+      }
+      else
+      {
+        cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl;
+        total = trans;
+      }
+    }
+    cout << total.bookNo << " " << total.units_sold << " " << total.revenue << endl;
+  } 
+  else
+  {
+    cerr << "Where data?!" << endl;
+    return -1;
+  }
+  return 0;
+}
+
+
