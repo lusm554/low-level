92549e38b6e73518709c6e01bf2522a630af3489
lusm <loveyousomuch554@gmail.com>
2022-09-24 16:18:16 +0300

    added constructors to Person

--- cp7-classes/7-1-4-section.cpp
+++ cp7-classes/7-1-4-section.cpp
@@ -9,7 +9,47 @@ using std::istream;
 using std::ostream;
 
 struct Sales_data;
+class Person;
 istream& read(istream &is, Sales_data& item);
+istream& Read(istream &is, Person& item);
+
+class Person
+{
+    public:
+        Person() = default;
+        Person(const string name, const string address) : 
+            name_(name), address_(address) {}
+        Person(istream &is)
+        {
+            Read(is, *this);
+        }
+
+        string get_name() const
+        {
+            return name_;
+        }
+
+        string get_address() const
+        {
+            return address_;
+        }
+    
+    //private:
+        string name_;
+        string address_;
+};
+
+istream& Read(istream &is, Person &obj)
+{
+    is >> obj.name_ >> obj.address_;
+    return is;
+}
+
+ostream& Print(ostream &os, Person &obj)
+{
+    os << obj.name_ << ' ' << obj.address_ << endl;
+    return os;
+}
 
 struct Sales_data
 {
