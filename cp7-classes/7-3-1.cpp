f529359027389777b0958bddb5126e5b4aaf09d9
lusm <loveyousomuch554@gmail.com>
2022-10-08 21:13:00 +0300

    move, set and display methods

--- cp7-classes/7-3-1.cpp
+++ cp7-classes/7-3-1.cpp
@@ -7,20 +7,50 @@ class Screen
     typedef std::string::size_type pos;
 
     Screen() = default;
-    Screen(post ht, post wd) : height(ht), width(wd), contents(ht * wd, ' ') {}
+    Screen(pos ht, pos wd) : height(ht), width(wd), contents(ht * wd, ' ') {}
     Screen(pos ht, pos wd, char c) : height(ht), width(wd), contents(ht * wd, c) {}
+
     char get() const
     {
       return contents[cursor];
     }
     inline char get(pos ht, pos wd) const;
+
+    Screen &set(char c)
+    {
+      contents[cursor] = c;
+      return *this;
+    }
+    Screen &set(pos r, pos col, char ch)
+    {
+      contents[r * width + col] = ch;
+      return *this;
+    }
+
     Screen &move(pos r, pos c);
+
     void some_member() const
     {
       ++access_cnt;
     }
 
+    Screen &display(std::ostream &os)
+    {
+      do_display(os);
+      return *this;
+    }
+    const Screen &display(std::ostream &os) const
+    {
+      do_display(os);
+      return *this;
+    }
+
   private:
+    void do_display(std::ostream &os) const
+    {
+      os << contents << std::endl;
+    }
+
     pos cursor = 0;
     pos height = 0, width = 0;
     std::string contents;
@@ -51,5 +81,8 @@ int main(void)
 
 void test_Screen(void)
 {
-  Screen obj(10, 10, 'g');
+  //Screen obj(10, 10, 'g');
+  Screen obj(5, 5, 'X');
+	obj.move(4, 0).set('#').display(std::cout);
+	obj.display(std::cout);   
 }
