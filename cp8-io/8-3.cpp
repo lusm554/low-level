3906f8f48a43e63de7f2ea5c47b152501ad68abc
lusm <loveyousomuch554@gmail.com>
2022-10-18 00:18:20 +0300

    use an istringstream to read each line as input

--- cp8-io/8-3.cpp
+++ cp8-io/8-3.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <sstream>
+#include <vector>
 
-
-int main(void)
+void read_iss(void)
 {
   std::string line;
   while (std::getline(std::cin, line))
@@ -12,6 +12,35 @@ int main(void)
     while (record >> str)
       std::cout << str << std::endl;
   }
+} 
+
+void read(std::istream& is)
+{
+  std::string line;
+  std::vector<std::string> lines;
+
+  // read input line by line
+  while (std::getline(is, line))
+  {
+    lines.push_back(line); 
+  }
+
+  // read each line like an input
+  for (std::string& iline : lines) 
+  {
+    std::istringstream record(iline);
+    std::string input;
+
+    while (record >> input)
+      std::cout << input << std::endl;
+  }
+}
+
+int main(void)
+{
+  //read_iss(); 
+  
+  read(std::cin);
     
   return 0;
 }
