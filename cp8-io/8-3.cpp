96dfe11c19da08c76510e7a4e209d143c345fc05
lusm <loveyousomuch554@gmail.com>
2022-10-18 01:45:20 +0300

    phone prog

--- cp8-io/8-3.cpp
+++ cp8-io/8-3.cpp
@@ -36,12 +36,44 @@ void read(std::istream& is)
   }
 }
 
+struct PersonInfo {
+  std::string name;
+  std::vector<std::string> phones;
+};
+
+void read_pers(std::istream& in)
+{
+  std::vector<PersonInfo> people;
+
+  // get input people
+  std::string line, word;
+  while (std::getline(in, line))
+  {
+    PersonInfo info;
+    std::istringstream record(line);
+    record >> info.name;
+    while (record >> word)
+      info.phones.push_back(word);
+    people.push_back(info);
+  }
+
+  // print peps from struct
+  for (const auto& info : people)
+  {
+    std::cout << info.name << " "; 
+    for (const std::string& phone : info.phones)
+      std::cout << phone << " ";
+    std::cout << std::endl;
+  }
+}
+
 int main(void)
 {
   //read_iss(); 
-  
-  read(std::cin);
+  //read(std::cin);
     
+  read_pers(std::cin);  
+
   return 0;
 }
 
